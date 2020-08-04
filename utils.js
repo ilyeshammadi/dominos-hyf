33f4db2827c9bea229ab8a0212f955066138f9c5
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2020-08-05 00:12:57 +0200

    Init

---
+++ utils.js
@@ -0,0 +1,12 @@
+module.exports = {
+  showBoard: (playerA, playerB, domBox, gameTable) => {
+    console.log("Player A:");
+    console.log(playerA);
+    console.log("Player B:");
+    console.log(playerB);
+    console.log("DomBox:");
+    console.log(domBox);
+    console.log("Table:");
+    console.log(gameTable);
+  },
+};
